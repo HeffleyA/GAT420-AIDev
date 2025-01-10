a65b2665bd61eaa79101807b64e93529b3d36f14
HeffleyA <aheffley@student.neumont.edu>
2025-01-10 13:54:58 -0700

    GOL

--- Source/Cells.h
+++ Source/Cells.h
@@ -6,7 +6,7 @@ class Cells
 {
 public:
 	Cells(int width, int height) : m_width{ width }, m_height{ height } { m_data.resize(width * height); }
-	const T& Read(int x, int y) const { return m_data[x + (y * m_width)]; }
+	const T Read(int x, int y) const { return m_data[x + (y * m_width)]; }
 	void Write(int x, int y, const T& v) { m_data[x + (y * m_width)] = v; }
 
 public:
